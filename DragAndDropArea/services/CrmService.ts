75800d25c0274bef231606aef905bd8deebd958b
Artur Sahakyan <artursahakyan2019@mail.ru>
2022-08-29 17:31:59 +0400

    Set 'error or alert' Dialog

--- DragAndDropArea/services/CrmService.ts
+++ DragAndDropArea/services/CrmService.ts
@@ -3,24 +3,26 @@ import FileHelper from '../helpers/FileHelper';
 
 let _context: ComponentFramework.Context<IInputs>;
 
+const notificationOption = {
+  errosCount: 0,
+  importedSucsessCount: 0,
+  details: '',
+  message: '',
+};
+
 export default {
   setContext(context: ComponentFramework.Context<IInputs>) {
     _context = context;
-  },
-
-  async uploadFiles(files: FileList) {
-    for (const file of Array.from(files)) {
-      await this.uploadFile(file);
-    }
   },
 
   async uploadFile(file: File) {
     try {
       const buffer: ArrayBuffer = await FileHelper.readFileAsArrayBufferAsync(file);
       const body: string = FileHelper.arrayBufferToBase64(buffer);
-
       // @ts-ignore
       const { entityTypeName, entityId } = _context.page;
+      const entityMetadata =
+      await _context.utils.getEntityMetadata(entityTypeName, entityId);
 
       const data: any = {
         'subject': '',
@@ -29,17 +31,43 @@ export default {
         'objecttypecode': entityTypeName,
       };
 
-      data[`objectid_${entityTypeName}@odata.bind`] = `/${entityTypeName}s(${entityId})`;
+      data[`objectid_${entityTypeName}@odata.bind`] =
+      `/${entityMetadata.EntitySetName}(${entityId})`;
 
       await _context.webAPI.createRecord('annotation', data);
+      notificationOption.importedSucsessCount += 1;
     }
     catch (ex: any) {
       console.error(ex.message);
+      notificationOption.message = ex.message;
+      notificationOption.details += `
+      File Name -${file.name}
+      Error message ${ex.message}`;
+      notificationOption.errosCount += 1;
     }
   },
 
   refreshTimeline() {
     // @ts-ignore
-    parent.Xrm.Page.getControl('Timeline').refresh();
+    parent.Xrm.Page.getControl('Timeline')?.refresh();
+  },
+
+  showNotificationPopup() {
+    if (notificationOption.errosCount === 0) {
+      const message = notificationOption.importedSucsessCount === 1
+        ? `${notificationOption.importedSucsessCount} file imported successfully`
+        : `${notificationOption.importedSucsessCount} files imported successfully`;
+
+      _context.navigation.openAlertDialog({ text: message });
+      notificationOption.importedSucsessCount = 0;
+    }
+    else {
+      notificationOption.errosCount > 1 ? notificationOption.message += ` 
+       ${notificationOption.errosCount} errors`
+        : notificationOption.message += ` ${notificationOption.errosCount} error`;
+
+      _context.navigation.openErrorDialog(notificationOption);
+      notificationOption.errosCount = 0;
+    }
   },
 };
