52599b8cc1d413f0f6518ab8f2249bdc1edcc85e
Artur-Sahakyan <90428984+Artur-Sahakyan@users.noreply.github.com>
2022-09-12 10:46:00 +0400

    add spinner (#1)
    * added spinner
    * added static "Title & Description" for Notes
    * added note import notifications

--- DragAndDropArea/components/FileUploadArea.tsx
+++ DragAndDropArea/components/FileUploadArea.tsx
@@ -1,18 +1,30 @@
 import * as React from 'react';
-
+import { Spinner, SpinnerSize } from '@fluentui/react/lib/Spinner';
 import CrmService from '../services/CrmService';
+import { Icon } from '@fluentui/react/lib/Icon';
 
 export interface IFileUploadAreaProps { }
 
 interface IFileUploadAreaState {
   dragCounter: number;
+  isLoading: boolean;
+  importedFilesCount: number
+  filesCount: number;
+  isDisabled: boolean;
+  isRenderedOneTime: boolean;
 }
 
 export class FileUploadArea extends React.Component<IFileUploadAreaProps, IFileUploadAreaState> {
   constructor(props: IFileUploadAreaProps) {
     super(props);
+
     this.state = {
       dragCounter: 0,
+      isLoading: false,
+      importedFilesCount: 0,
+      filesCount: 0,
+      isDisabled: true,
+      isRenderedOneTime: false,
     };
   }
 
@@ -31,29 +43,51 @@ export class FileUploadArea extends React.Component<IFileUploadAreaProps, IFileU
   async drop(event: React.DragEvent) {
     event.preventDefault();
     const files = event.dataTransfer?.files;
-    await CrmService.uploadFiles(files);
-
-    this.setState({ dragCounter: 0 });
+    this.setState({ isLoading: true, filesCount: Array.from(files).length });
+    for (const file of Array.from(files)) {
+      await CrmService.uploadFile(file, Array.from(files).length);
+      this.setState({ importedFilesCount: this.state.importedFilesCount + 1 });
+    }
+    this.setState({ dragCounter: 0, isLoading: false, importedFilesCount: 0 });
     CrmService.refreshTimeline();
   }
 
+  async checkNotes() {
+    const hasNotes = await CrmService.hasNotes();
+    this.setState({ isDisabled: hasNotes, isRenderedOneTime: true });
+  }
+
   public render(): React.ReactNode {
+    const { filesCount, importedFilesCount, isLoading, isDisabled } = this.state;
+    if (!this.state.isRenderedOneTime) this.checkNotes();
+
     return (
       <div className="draganddroparea">
-        <div className="container">
-          <div
-            onDragOver={this.dragOver.bind(this)}
-            onDragEnter={this.dragEnter.bind(this)}
-            onDragLeave={this.dragLeave.bind(this)}
-            onDrop={this.drop.bind(this)}
-            className={`dropArea 
+        { isDisabled
+          ? <div className="container">
+            <div
+              onDragOver={this.dragOver.bind(this)}
+              onDragEnter={this.dragEnter.bind(this)}
+              onDragLeave={this.dragLeave.bind(this)}
+              onDrop={this.drop.bind(this)}
+              className={`dropArea 
               ${this.state.dragCounter > 0 ? 'dropAreaHover' : ''}`}
-          >
-            <div className="textIcon">
-              <div className="text"><p>Drag and drop files here to upload</p></div>
+            >{isLoading
+                ? <>
+                  <div className="textIcon">
+                    <p className='spinnerText'>
+                      { `importing ${importedFilesCount} of ${filesCount} files` }</p>
+                    <Spinner size={SpinnerSize.large} />
+                  </div>
+                </>
+                : <div className={'text'}>
+                  <p> Drag and drop files here to upload</p></div>}
             </div>
+          </div> : <div className='errorContainer'>
+            <Icon className='errorIcone' iconName="error"></Icon>
+            <p className='errorMessage'>Notes (including attachments) are disabled</p>
           </div>
-        </div>
+        }
       </div>
     );
   }
--- DragAndDropArea/services/CrmService.ts
+++ DragAndDropArea/services/CrmService.ts
@@ -3,43 +3,87 @@ import FileHelper from '../helpers/FileHelper';
 
 let _context: ComponentFramework.Context<IInputs>;
 
+const notificationOptions = {
+  errorsCount: 0,
+  importedSucsessCount: 0,
+  filesCount: 0,
+  details: '',
+  message: '',
+};
+
 export default {
   setContext(context: ComponentFramework.Context<IInputs>) {
     _context = context;
   },
 
-  async uploadFiles(files: FileList) {
-    for (const file of Array.from(files)) {
-      await this.uploadFile(file);
-    }
+  async getEntitySetName(entityTypeName: string) {
+    const entityMetadata = await _context.utils.getEntityMetadata(entityTypeName);
+    return entityMetadata.EntitySetName;
   },
 
-  async uploadFile(file: File) {
+  async hasNotes() {
+    // @ts-ignore
+    const contextPage = _context.page;
+    const entityMetadataResponse =
+     await fetch(`${contextPage.getClientUrl()}/api/data/v9.0/EntityDefinitions(LogicalName='${contextPage.entityTypeName}')`);
+    const entityMetadata = await entityMetadataResponse.json();
+    return entityMetadata.HasNotes;
+  },
+
+  async uploadFile(file: File, filesCount: number) {
     try {
+      notificationOptions.filesCount = filesCount;
       const buffer: ArrayBuffer = await FileHelper.readFileAsArrayBufferAsync(file);
       const body: string = FileHelper.arrayBufferToBase64(buffer);
-
       // @ts-ignore
       const { entityTypeName, entityId } = _context.page;
+      const entitySetName = await this.getEntitySetName(entityTypeName);
 
       const data: any = {
-        'subject': '',
+        'subject': _context.parameters.noteTitle.raw,
+        'notetext': _context.parameters.noteDescription.raw,
         'filename': file.name,
         'documentbody': body,
         'objecttypecode': entityTypeName,
       };
 
-      data[`objectid_${entityTypeName}@odata.bind`] = `/${entityTypeName}s(${entityId})`;
+      data[`objectid_${entityTypeName}@odata.bind`] = `/${entitySetName}(${entityId})`;
 
       await _context.webAPI.createRecord('annotation', data);
+      notificationOptions.importedSucsessCount += 1;
     }
     catch (ex: any) {
       console.error(ex.message);
+      notificationOptions.details += `File Name ${file.name} Error message ${ex.message}`;
+      notificationOptions.errorsCount += 1;
     }
   },
 
   refreshTimeline() {
     // @ts-ignore
-    parent.Xrm.Page.getControl('Timeline').refresh();
+    parent.Xrm.Page.getControl('Timeline')?.refresh();
+    this.showNotificationPopup();
+  },
+
+  showNotificationPopup() {
+    if (notificationOptions.errorsCount === 0) {
+      const message = notificationOptions.importedSucsessCount > 1
+        ? `${notificationOptions.importedSucsessCount} of ${notificationOptions.importedSucsessCount} files imported successfully`
+        : `${notificationOptions.importedSucsessCount} of ${notificationOptions.importedSucsessCount} file imported successfully`;
+
+      _context.navigation.openConfirmDialog({ text: message });
+      notificationOptions.importedSucsessCount = 0;
+    }
+    else {
+      notificationOptions.message = notificationOptions.errorsCount > 1
+        ? `${notificationOptions.errorsCount} 
+        of ${notificationOptions.filesCount} files errored during import`
+        : `${notificationOptions.errorsCount} 
+        of ${notificationOptions.filesCount} file errored during import`;
+
+      _context.navigation.openErrorDialog(notificationOptions);
+      notificationOptions.errorsCount = 0, notificationOptions.importedSucsessCount = 0;
+      notificationOptions.details = '';
+    }
   },
 };
